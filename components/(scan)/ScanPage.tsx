78b9cfe57885cb0827ec76c46759da1a2c9f530c
MartinPettersen <searchingtheskyfor@gmail.com>
2024-07-04 14:21:47 +0200

    add api call for product information

--- components/(scan)/ScanPage.tsx
+++ components/(scan)/ScanPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { CameraView, Camera } from "expo-camera";
 import ScanButton from "./ScanButton";
+import { useGetProductInformation } from "../../hooks/useGetProductInformation";
 
 type ScanProps = {
   type: string;
@@ -9,41 +10,40 @@ type ScanProps = {
 };
 
 const ScanPage = () => {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState(true);
+  const [ean, setEan] = useState<string | null>(null);
+  // const productInfo = useGetProductInformation(ean);
 
-    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-    const [scanned, setScanned] = useState(true);
-    const [ean, setEan] = useState<string | null>(null)
-  
-    useEffect(() => {
-      const getCameraPermissions = async () => {
-        const { status } = await Camera.requestCameraPermissionsAsync();
-        setHasPermission(status === "granted");
-      };
-  
-      getCameraPermissions();
-    }, []);
-  
-    const resetScanner = () => {
-      setEan(null)
-      setScanned(false)
-    }
-  
-    const handleBarCodeScanned = ({ type, data }: ScanProps) => {
-      setEan(data)
-      setScanned(true);
-  
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
     };
-  
-    if (hasPermission === null) {
-      return <Text>Ber om kamera tilgang</Text>;
-    }
-    if (hasPermission === false) {
-      return <Text>Mangler Kamera tilgang</Text>;
-    }
-  
+
+    getCameraPermissions();
+  }, []);
+
+  const resetScanner = () => {
+    setEan(null);
+    setScanned(false);
+  };
+
+  const handleBarCodeScanned = ({ type, data }: ScanProps) => {
+    setEan(data);
+    setScanned(true);
+  };
+
+
+
+  if (hasPermission === null) {
+    return <Text>Ber om kamera tilgang</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Mangler Kamera tilgang</Text>;
+  }
 
   return (
-
     <View style={styles.container}>
       <CameraView
         onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
@@ -53,26 +53,27 @@ const ScanPage = () => {
         style={StyleSheet.absoluteFillObject}
       />
       <View style={styles.buttonContainer}>
-
-      {scanned? <ScanButton buttonText="Skan" action={() => resetScanner()}/> : <ScanButton buttonText="Skanner..." action={() => setScanned(true)}/>}
-        </View>
+        {scanned ? (
+          <ScanButton buttonText="Skan" action={() => resetScanner()} />
+        ) : (
+          <ScanButton buttonText="Skanner..." action={() => setScanned(true)} />
+        )}
+      </View>
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "flex-end",
-    },
-    buttonContainer: {
-      flexDirection: "column",
-      justifyContent: "space-around",
-      alignItems: "center",
-      paddingVertical: 20,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  buttonContainer: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+});
 
 export default ScanPage;
---
+++ hooks/useGetProductInformation.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export const useGetProductInformation = (ean: string) => {
+  const [productInformation, setProductInformation] = useState(null);
+
+
+  useEffect(() => {
+    const fetchProductInformation = async () => {
+      try {
+        const url = `https://kassal.app/api/v1/products/ean/${ean}`;
+        const options = {
+          headers: {
+            "Authorization": `Bearer ${process.env.EXPO_PUBLIC_KASSAL_KEY}`,
+          },
+        };
+
+        const res = await fetch(url, options);
+        const data = await res.json();
+
+
+        setProductInformation(data.data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (ean) {
+      fetchProductInformation();
+    }
+  }, [ean]);
+
+  return productInformation;
+};
