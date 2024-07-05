4f37d3d6f50170e62afbeb8c6740a8b5c1e72152
MartinPettersen <searchingtheskyfor@gmail.com>
2024-07-05 09:01:50 +0200

    add navigation

--- App.tsx
+++ App.tsx
@@ -2,10 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import ScanScreen from './screens/ScanScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <ScanScreen />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Scan" component={ScanScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
